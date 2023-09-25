c56034dd8f566772d75c73db96f09760c5edf12c
husaab <126218851+husaab@users.noreply.github.com>
2023-09-25 17:00:57 -0400

    Contains Duplicate Problem

---
+++ ContainsDuplicate.py
@@ -0,0 +1,12 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+
+        storenums= set() #creating a set to store numbers
+
+        for num in nums:        #looping through nums array
+            if num in storenums:    #check if num is already in set
+                return True #if so, return true
+            else:   #else, add it to set
+                storenums.add(num)
+        
+        return False    #by the end of the for loop, if no num was in set, then return False
